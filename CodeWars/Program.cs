54a4d62ef4ad05c40a6fc9e89925ab252f16303a
Wojciech Pakoca <wojtekpako2@gmail.com>
2023-05-12 15:33:02 +0200

    social media likes

--- CodeWars/Program.cs
+++ CodeWars/Program.cs
@@ -278,4 +278,26 @@ public static class Kata
         string[] names = name.Split(' ');
         return string.Format("{0}.{1}", names[0].First(), names[1].First()).ToUpper();
     }
+
+    public static int SquareDigits(int n)
+    {
+        string result = "";
+        foreach (char c in n.ToString())
+        {
+            result += Math.Pow(int.Parse(c.ToString()), 2);
+        }
+        return int.Parse(result);
+    }
+
+    public static string Likes(string[] name)
+    {
+        return name.Length switch
+        {
+            0 => "no one likes this",
+            1 => string.Format("{0} likes this", name[0]),
+            2 => string.Format("{0} and {1} like this", name[0], name[1]),
+            3 => string.Format("{0}, {1} and {2} like this", name[0], name[1], name[2]),
+            _ => string.Format("{0}, {1} and {2} others like this", name[0], name[1], name.Length - 2),
+        };
+    }
 }
